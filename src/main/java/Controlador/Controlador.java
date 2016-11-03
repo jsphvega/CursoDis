fc83d1d8395b473395835d3bfbd62aed2e1d764b
Joseph <j-vv01hotmail.com>
2016-11-03 00:08:05 -0600

    La prueba

--- src/main/java/Controlador/Controlador.java
+++ src/main/java/Controlador/Controlador.java
@@ -1,12 +1,18 @@
 package Controlador;
 
+import java.io.FileNotFoundException;
 import java.util.*;
 
 import DTO.DTOArchivoCategoria;
 import DTO.DTOArchivoConversacion;
 import DTO.DTOEntrenamientoTutor;
 import DTO.DTORespuestaSatisfactoria;
+import Integracion.Conversacion;
+import Integracion.Traductor;
 import Negocios.Consulta;
+import Negocios.EntradaVoz;
+import Negocios.FabricaDocumento;
+import Negocios.Informacion;
 
 public class Controlador {
 
@@ -18,24 +24,60 @@ public class Controlador {
     }
 
     public void traducirRespuestas(Consulta pConsulta) {
-	// TODO implement here
+	Traductor sTraductor = new Traductor();
+	sTraductor.autenticarServicio();
+
+	System.out.println(sTraductor.traducirTexto(pConsulta.getPregunta()));
     }
 
     public void agregarRespuestasPreguntas(DTOEntrenamientoTutor pEntrenamiento) {
-	// TODO implement here
+	Conversacion cc;
+	cc = Conversacion.getInstance();
+	if (cc.agregarInformacion(pEntrenamiento.getPregunta(), pEntrenamiento.getRespuesta(),
+		pEntrenamiento.getPregunta()))
+	    System.out.println("Si ingresa");
     }
 
-    public void grabarAudio() {
-	// TODO implement here
+    public String grabarAudio() {
+	try {
+	    EntradaVoz sEntrada = new EntradaVoz();
+	    return sEntrada.procesarPregunta();
+	} catch (Exception e) {
+	    return "No se reconoce";
+	}
     }
 
     public String obtenerRespuestas(Consulta pConsulta) {
-	// TODO implement here
-	return "";
+	Conversacion sConversacion;
+	sConversacion = Conversacion.getInstance();
+	ArrayList<Informacion> listaConversacion = sConversacion.consultarInformacion(pConsulta.getPregunta());
+
+	if (listaConversacion.size() == 0)
+	    return "No hay";
+	else {
+	    String sTexto = "";
+	    for (int i = 0; i < listaConversacion.size(); i++)
+		sTexto += listaConversacion.get(i).getDescripcion() + "\n";
+	    return sTexto;
+	}
+
     }
 
     public void crearDocumentoConversacion(DTOArchivoConversacion pArchivoConversacion) {
-	// TODO implement here
+	ArrayList<Consulta> listaConsultas = new ArrayList<>();
+
+	Consulta sConsulta1 = new Consulta("Cuanto costo la almohada");
+	sConsulta1.agregarRespuesta("Costo 5000 colones");
+	sConsulta1.agregarRespuesta("O tal vez costo mas");
+
+	Consulta sConsulta2 = new Consulta("Cuantos pedos se tiro anoche");
+	sConsulta2.agregarRespuesta("Todos, me desmaye y no pude contar");
+
+	listaConsultas.add(sConsulta1);
+	listaConsultas.add(sConsulta2);
+
+	FabricaDocumento sFabrica = new FabricaDocumento();
+	sFabrica.crearDocumento(pArchivoConversacion.getTipo(), listaConsultas);
     }
 
     public void guardarIncorrectas(ArrayList<Consulta> pIncorrectas) {
@@ -51,8 +93,25 @@ public class Controlador {
 	return false;
     }
 
-    public String prueba(){
-	return "Si";
+    public static void main(String[] args) throws ClassNotFoundException, FileNotFoundException, InterruptedException {
+	Controlador sControlador = new Controlador();
+
+	/*Consulta pConsulta = new Consulta("Mariposa");
+	sControlador.traducirRespuestas(pConsulta);*/
+
+	/*DTOArchivoConversacion sDTOConversacion = new	DTOArchivoConversacion(); 
+	sDTOConversacion.setTipo("PDF");
+	sDTOConversacion.setTipo("TXT");
+	sControlador.crearDocumentoConversacion(sDTOConversacion);*/
+
+	/*DTOEntrenamientoTutor sDTOTutor = new DTOEntrenamientoTutor();
+	sDTOTutor.setPregunta("#Diseño"); 
+	sDTOTutor.setRespuesta("Esto es una prueba de Diseño"); 
+	sControlador.agregarRespuestasPreguntas(sDTOTutor);*/
+	
+	Consulta pConsulta = new Consulta(sControlador.grabarAudio());
+	System.out.println(sControlador.obtenerRespuestas(pConsulta));
+
+	
     }
-    
 }
